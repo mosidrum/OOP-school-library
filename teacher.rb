dbe6e7cb8c5a981b4a12538274999a54781212e1
mosidrum <mosiokanga@gmail.com>
2023-09-04 13:57:37 +0100

    Code review changes

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name, permission: true)
+  def initialize(age, name, specialization, permission: true)
     super(age, name, parent_permission: permission)
     @specialization = specialization
   end
