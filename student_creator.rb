be202f73a04c75991a13ac80840a91932950cd37
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-04 11:43:47 +0000

    add another creator method to make it a single responsibility

---
+++ student_creator.rb
@@ -0,0 +1,20 @@
+require_relative 'student'
+
+class StudentCreator
+    def create
+        print 'Enter the name of the student: '
+    name = gets.chomp
+    print 'Enter the age of the student: '
+    age = gets.chomp
+    print 'Does the student have parent permission? [Y/N]: '
+    parent_permission = gets.chomp
+    if parent_permission.downcase == 'y'
+      student = Student.new(age, name, false)
+    elsif parent_permission.downcase == 'n'
+      student = Student.new(age, name, true)
+    else
+      puts 'Invalid input'
+      return
+    end
+    end
+end
---
+++ teacher_creator.rb
@@ -0,0 +1,12 @@
+require_relative 'teacher'
+class TeacherCreator
+    def create
+    print 'Enter name of the teacher: '
+    name = gets.chomp
+    print 'Enter the age of the teacher: '
+    age = gets.chomp.to_i
+    print 'Enter his/her specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    end
+end
