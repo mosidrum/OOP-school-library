1a4eeb8b0e7bfb5eda8aa1f9075a486b9223e924
mosidrum <mosiokanga@gmail.com>
2023-09-05 15:03:05 +0100

    write to json file

--- app.rb
+++ app.rb
@@ -4,17 +4,28 @@ require_relative 'classroom'
 require_relative 'rental_creator'
 require_relative 'teacher_creator'
 require_relative 'book_creator'
-class App
+require_relative 'file_writer'
+require 'json'
+class App < FileWriter
   def initialize
     @rentals = []
     @people = []
     @books = []
     @book_index = 0
+    # load_data_from_files
   end
+
+  # def load_data_from_files
+  #   @books = FileLoader.load_books
+  #   @people = FileLoader.load_people
+  #   @rentals =FileLoader.load_rentals(@people, @books)
+  #   @book_index = @books.map(&:index).max.to_i + 1
+  # end
 
   def create_book(title, author)
     book = Book.new(title, author, @book_index)
     @books << book
+    write_book_to_file(book)
     @book_index += 1
     book
   end
@@ -22,12 +33,14 @@ class App
   def create_teacher(name, age, specialization)
     teacher = Teacher.new(name, age, specialization)
     @people << teacher
+    write_person_to_file(teacher)
     teacher
   end
 
   def create_student(name, age, has_parent_permission)
     student = Student.new(age, name, has_parent_permission)
     @people << student
+    write_person_to_file(student)
     student
   end
 
@@ -36,6 +49,7 @@ class App
     person.add_rental(rental)
     book.add_rental(person, rental)
     @rentals << rental
+    write_rental_to_file(rental)
     rental
   end
 
@@ -57,4 +71,29 @@ class App
       puts "Book: #{rent.book.title} by #{rent.book.author} was rented on Date: #{rent.date}"
     end
   end
+
+  # def book_to_json(book)
+  #   {
+  #     "index": book.index,
+  #     "author": book.author,
+  #     "title": book.title
+  #   }.to_json
+  # end
+  #
+  # def person_to_json(person)
+  #   {
+  #     "id": person.id,
+  #     "name": person.name,
+  #     "age": person.age,
+  #     "class": person.class
+  #   }.to_json
+  # end
+  #
+  # def rental_to_json(rental)
+  #   {
+  #     "person": person_to_json(rental.person),
+  #     "book": book_to_json(rental.book),
+  #     "date": rental.date
+  #   }.to_json
+  # end
 end
