191d879552d48d22609dae1a3ee20581efafea82
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-06 15:17:54 +0000

    testing the capitalize decorator

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+require_relative '../capitalize_decorator'
+
+RSpec.describe CapitalizeDecorator do
+  describe '#correct_name' do
+    it 'handles the capitalize the correct_name' do
+      nameable = double(Nameable)
+      allow(nameable).to receive(:correct_name).and_return('jalloh')
+
+      decorator = CapitalizeDecorator.new(nameable)
+      result = decorator.correct_name
+
+      expect(nameable).to have_received(:correct_name)
+      expect(result).to eq('Jalloh')
+    end
+  end
+end
