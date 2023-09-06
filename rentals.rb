d92f7530b78c6cdb53b50b5e488bca3dc4a0a057
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-06 12:37:59 +0000

    adding some changes to make it work

--- rentals.rb
+++ rentals.rb
@@ -1,13 +1,27 @@
-# Class rentals
+require 'spec_helper'
+require_relative '../rentals'
+require_relative '../book'
+require_relative '../person'
 
-class Rentals
-  attr_accessor :date, :book, :person
+RSpec.describe Rentals do
+  let(:person) { Person.new(25, 'John Doe', parent_permission: true) }
+  let(:book) { Book.new('Sample Title', 'Sample Author', 1) }
+  let(:date) { '2023-09-01' }
+  let(:rental) { Rentals.new(person, book, date) }
 
-  def initialize(person, book, date)
-    @date = date
-    @person = person
-    @book = book
-    book.add_rental(person, self)
-    person.add_rental(self)
+  describe '#initialize' do
+    it 'creates a new rental with the correct attributes' do
+      expect(rental.date).to eq(date)
+      expect(rental.person).to eq(person)
+      expect(rental.book).to eq(book)
+    end
+
+    it 'adds the rental to the book' do
+      expect(book.rentals).to include(rental)
+    end
+
+    it 'adds the rental to the person' do
+      expect(person.rentals).to include(rental)
+    end
   end
 end
