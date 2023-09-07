10679354e9372bd588c258d4ad4b168bf3b59e88
Abdoul <abdoulramanediallo44@gmail.com>
2023-09-07 09:15:44 +0000

    adding test for initialize

--- spec/base_decorator_spec.rb
+++ spec/base_decorator_spec.rb
@@ -3,6 +3,15 @@ require 'spec_helper'
 require_relative '../base_decorator'
 
 RSpec.describe BaseDecorator do
+  describe '#initialize' do
+    it 'initializes with a nameable object' do
+      nameable = double('Nameable')
+      decorator = BaseDecorator.new(nameable)
+
+      expect(decorator.instance_variable_get(:@nameable)).to eq(nameable)
+    end
+  end
+
   describe '#correct_name' do
     it 'delegates the correct_name to the decorated object' do
       nameable = double('Nameable')
